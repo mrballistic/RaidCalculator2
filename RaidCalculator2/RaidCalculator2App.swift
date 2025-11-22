ab5904b6fff45db96b838d07d548c7cfb977f01e
Todd Greco <mrballistic@gmail.com>
2025-11-22 11:34:39 -0800

    Initial Commit

---
+++ RaidCalculator2/RaidCalculator2App.swift
@@ -0,0 +1,17 @@
+//
+//  RaidCalculator2App.swift
+//  RaidCalculator2
+//
+//  Created by todd.greco on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct RaidCalculator2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RaidCalculator2Tests/RaidCalculator2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  RaidCalculator2Tests.swift
+//  RaidCalculator2Tests
+//
+//  Created by todd.greco on 11/22/25.
+//
+
+import Testing
+@testable import RaidCalculator2
+
+struct RaidCalculator2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
